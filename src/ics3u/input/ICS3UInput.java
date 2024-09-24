d1edc54156e1b6765000ddc94197865331ef37de
michael.roy-diclemen <michael.roy-diclemen@TEACH2D-C3NYBS3.EDU.YRDSB.NET>
2024-09-24 08:21:27 -0400

    Initial Commit

---
+++ src/ics3u/input/ICS3UInput.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ics3u.input;
+
+/**
+ * Exercises - Input
+ * @author michael.roy-diclemen
+ */
+public class ICS3UInput {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    /***************************************************************************
+    * QUESTION 1: List the methods for inputting each of the primitive types using the Scanner class.
+    * ANSWER 1:
+    * 
+    * QUESTION 2: 2.	What is the best way to input a String? Try it and Explain.
+    * ANSWER 2:
+    ***************************************************************************/
+    
+    //Programming Question 1
+    
+    
+    //Programming Question 2
+    
+    
+    //Programming Question 3
+    
+    
+    //Programming Question 4
+    
+    
+    //Programming Question 5
+    
+    
+    //Programming Question 6
+    
+    }
+    
+}
